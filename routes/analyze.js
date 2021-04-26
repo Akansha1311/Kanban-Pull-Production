75914160c9c0dd98b040d49634526b778f9df953
Akansha1311 <78498579+Akansha1311@users.noreply.github.com>
2021-04-26 11:11:44 +0530

    Add files via upload

---
+++ routes/analyze.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const path = require("path");
+let PerformanceAnalyze = require("../models/PerformanceAnalyze");
+let moment = require("moment");
+
+router.get("/", async (req, res) => {
+  let fabric = await PerformanceAnalyze.find({ dept: "fabric" }, "-_id -__v")
+    .sort({ date: 1 })
+    .lean();
+  let cutting = await PerformanceAnalyze.find({ dept: "cutting" }, "-_id -__v")
+    .sort({ dateOfCutting: 1 })
+    .lean();
+  let sewing = await PerformanceAnalyze.find({ dept: "sewing" }, "-_id -__v")
+    .sort({ dateOfSewing: 1 })
+    .lean();
+  let finishing = await PerformanceAnalyze.find(
+    { dept: "finishing" },
+    "-_id -__v"
+  )
+    .sort({ dateOfFinishing: 1 })
+    .lean();
+  res.render(path.join(__dirname, "../", "/views/analyze"), {
+    fabric,
+    cutting,
+    sewing,
+    finishing,
+    moment,
+  });
+});
+
+module.exports = router;
